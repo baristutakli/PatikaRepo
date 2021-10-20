b22fd32b59892c6eb15b2eada9e0472370a10cb2
baristutakli <baristutakli@hotmail.com>
2021-10-20 14:07:21 +0300

    Task Management Form added

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 
 This repository contains the codes that i wrote for the solutions of tasks given during the trainings on <a href="https://app.patika.dev/">Patika</a>.
 
-* test.js file is created to learn form and form management. I blocked default form submit. Then, ı got score and added it to localstorage.
+* test.js file is created to learn form and form management. I blocked default form submit. Then, ı got the score information and added it to localstorage.
--- js/test.js
+++ js/test.js
@@ -1,6 +1,44 @@
 
 document.body.style.backgroundColor = "#99CCFF"
 
+let formDom = document.querySelector("#userForm")
+formDom.addEventListener("submit", formHandler)
+
+
+function formHandler(event){
+    event.preventDefault()
+
+    const userName = document.querySelector("#username")
+    const task = document.querySelector("#task")
+    const subTasks = document.querySelector("#subTasks")
+    const name = document.querySelector("#Uname")
+    
+    if (userName.value && task.value && subTasks.value ) {
+        name.innerHTML = userName.value
+        addItem(task.value,subTasks.value)
+        name.innerHTML = ""
+        task.value = ""
+        subTasks.value = ""
+        alert("Task is added!")
+
+    } else {
+        alert("Hey! You forgot something. All fields should be completed")
+    }
+
+}
+let userListDom = document.querySelector("#userList")
+function addItem(task,subTasks){
+    let liDom = document.createElement("li")
+    liDom.innerHTML = `
+    ${task}
+    <span class="badge badge-primary badge-pill">${subTasks}</span>
+    `
+    liDom.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center')
+    userListDom.append(liDom)
+}
+
+
+/*
 let formDom =document.querySelector("#userForm")
 formDom.addEventListener("submit",formSubmit)
 function formSubmit(event){
@@ -10,7 +48,7 @@ function formSubmit(event){
     let scoreInpDom = document.querySelector("#score")
     console.log(scoreInpDom.value)
 
-    /* use localStorage
+    // use localStorage
      localStorage.setItem("score",scoreInpDom.value)
-     */
-}
+     
+}*/
