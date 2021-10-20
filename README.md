ce7abdc1f579625406cc31c05f72ab31f11af366
baristutakli <baristutakli@hotmail.com>
2021-10-20 11:20:50 +0300

    "form and form management"

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 This repository contains the codes that i wrote for the solutions of tasks given during the trainings on <a href="https://app.patika.dev/">Patika</a>.
 
+* test.js file is created to learn form and form management. I blocked default form submit. Then, ı got score and added it to localstorage.
--- js/test.js
+++ js/test.js
@@ -1,2 +1,16 @@
 
-document.body.style.backgroundColor = "grey"
+document.body.style.backgroundColor = "#99CCFF"
+
+let formDom =document.querySelector("#userForm")
+formDom.addEventListener("submit",formSubmit)
+function formSubmit(event){
+    event.preventDefault() // form submitte default 
+
+    // Getting infomation from form named UserForm
+    let scoreInpDom = document.querySelector("#score")
+    console.log(scoreInpDom.value)
+
+    /* use localStorage
+     localStorage.setItem("score",scoreInpDom.value)
+     */
+}
