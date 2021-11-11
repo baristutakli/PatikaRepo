0cec9003b9fe6b263306292412a9734dac54aa99
baristutakli <baristutakli@hotmail.com>
2021-11-11 20:25:09 +0300

    "fetch the data from a website"

--- js/fetchApp.js
+++ js/fetchApp.js
@@ -8,10 +8,22 @@ fetch("data/settings.json").then(
            console.log(responseJson.userName)
         })
 
-// Now, let's try to get information from a website.
+// Now, let's try to get information from a website, and added information to a table.
 
+let table = document.querySelector("#elements")
 fetch("https://jsonplaceholder.typicode.com/posts").then(
     response => response.json()
     .then(responseJson => {
-        console.log(responseJson)
+        responseJson.forEach(element => {
+            let tr = document.createElement("tr")
+            let thId = document.createElement("th")
+            thId.scope ="row"
+            thId.textContent = "" +element.userId
+            let tdTitle = document.createElement("td")
+            tdTitle.textContent  = element.title
+            let tdBody = document.createElement("td")
+            tdBody.textContent  = element.body.slice(0,50)+ "..."
+            tr.append(thId,tdTitle,tdBody)
+            table.append(tr)
+        });
     }))
